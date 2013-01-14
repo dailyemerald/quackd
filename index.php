aaeb7f60dd05935fd733461f2ba7da66452aaead
Ivar Vong <ivar@ivarvong.com>
2013-01-13 18:27:10 -0800

    headers for tag and serach

--- index.php
+++ index.php
@@ -12,6 +12,15 @@ get_header();
 	<?php if (is_home()) { ?>
 		<time id="top_time">Our newest essays</time>
 	<?php } ?>
+
+	<?php if (is_tag()) { ?>
+		<time id="top_time">Essays about <?php single_tag_title(); ?></time>
+	<?php } ?>
+
+	<?php if (is_search()) { ?>
+		<time id="top_time">Essays containing <?php the_search_query(); ?></time>
+	<?php } ?>
+
 </header>
 
 <?php
