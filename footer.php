21384b6335d5fa1a8a3bbe5c0338ce238e807c63
Ivar Vong <ivar@ivarvong.com>
2013-01-13 18:20:30 -0800

    remove GA -- it's already supported by the theme.

--- footer.php
+++ footer.php
@@ -546,21 +546,7 @@
 			    $("li.text_field").removeClass("active"), $("li.text_field").first().addClass("active")
 			});
 </script>
-		
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36905707-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-		
 		<?php wp_footer(); ?>
 	</body>
 </html>
